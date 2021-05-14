1fed3c95545553136f7f6931e0adb498d444cc72
Epic312 <kaden_ostendorf@yahoo.com>
2021-05-14 11:41:49 -0600

    small cantaloupes can be placed on floors

--- src/main/java/com/teamaurora/topsoil/common/block/CantaloupeBlock.java
+++ src/main/java/com/teamaurora/topsoil/common/block/CantaloupeBlock.java
@@ -4,10 +4,13 @@ import com.teamaurora.topsoil.core.registry.TopsoilBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.IGrowable;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.EnumProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -15,13 +18,22 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 public class CantaloupeBlock extends Block implements IGrowable {
+    public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
     protected static final VoxelShape SMALL_SHAPE = Block.makeCuboidShape(3.0D, 6.0D, 3.0D, 13.0D, 16.0D, 13.0D);
+    protected static final VoxelShape SMALL_SHAPE_BOTTOM = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 10.0D, 13.0D);
 
     public CantaloupeBlock(Properties properties) {
         super(properties);
+        this.setDefaultState(this.stateContainer.getBaseState().with(HALF, DoubleBlockHalf.UPPER));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder.add(HALF));
     }
 
     @Override
@@ -41,12 +53,25 @@ public class CantaloupeBlock extends Block implements IGrowable {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return SMALL_SHAPE;
+        return state.get(HALF) == DoubleBlockHalf.LOWER ? SMALL_SHAPE_BOTTOM : SMALL_SHAPE;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        Direction face = context.getFace();
+        if (face == Direction.DOWN) {
+            return getDefaultState();
+        }
+        if (face == Direction.UP) {
+            return getDefaultState().with(HALF, DoubleBlockHalf.LOWER);
+        }
+        return null;
     }
 
     @Override
     public boolean ticksRandomly(BlockState state) {
-        return true;
+        return state.get(HALF) == DoubleBlockHalf.UPPER;
     }
 
     @Override
